b77dab03175578a3b67f4da09220142e65f7dcef
jluisv987 <jluisv987@gmail.com>
2021-01-25 04:06:06 -0600

    Agregado middleware whitenoise y static_root

--- BeatZen/settings.py
+++ BeatZen/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -111,6 +112,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/imagenes/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
